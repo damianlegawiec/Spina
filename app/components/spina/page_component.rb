f273c4b1b30752b64a3f8c1c3a15bf53d6749ca2
Bram Jetten <mail@bramjetten.nl>
2020-10-29 23:12:28 +0100

    Page component

--- app/components/spina/page_component.rb
+++ app/components/spina/page_component.rb
@@ -5,6 +5,11 @@ module Spina
       @page = page
     end
 
+    def depth
+      return 0 unless view_context.is_a? Spina::PageListComponent
+      view_context.depth
+    end
+
     def children
       @children ||= @page.children.active.sorted.regular_pages
     end
--- app/components/spina/page_list_component.rb
+++ app/components/spina/page_list_component.rb
@@ -1,8 +1,10 @@
 module Spina
   class PageListComponent < ApplicationComponent
+    attr_reader :depth
 
-    def initialize(pages:)
+    def initialize(pages:, depth: 0)
       @pages = pages
+      @depth = depth
     end
 
   end
