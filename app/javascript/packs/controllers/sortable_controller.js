122680ffd38a0430401eb00ab236abca0127245f
Bram Jetten <mail@bramjetten.nl>
2020-10-29 23:22:11 +0100

    Order

--- app/javascript/packs/controllers/sortable_controller.js
+++ app/javascript/packs/controllers/sortable_controller.js
@@ -5,8 +5,17 @@ export default class extends Controller {
 
   connect() {
     this.sortable = Sortable.create(this.element, {
-      handle: '[data-sortable-handle]'
+      handle: '[data-sortable-handle]',
+      onEnd: this.sort.bind(this)
     })
+  }
+
+  sort(event) {
+    console.log(this.orderedIds)
+  }
+
+  get orderedIds() {
+    return this.sortable.toArray()
   }
 
 }
