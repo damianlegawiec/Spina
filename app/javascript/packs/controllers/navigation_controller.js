e655d8c9fc043d52d1a661aaeacb4d1b75cc702f
Bram Jetten <mail@bramjetten.nl>
2020-11-14 09:41:32 +0100

    Navigation

--- app/javascript/packs/controllers/navigation_controller.js
+++ app/javascript/packs/controllers/navigation_controller.js
@@ -1,14 +1,27 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "firstLevel", "secondLevel" ]
+  static targets = [ "firstLevel", "firstLevelButton", "secondLevel" ]
 
   connect() {
-    
+    // Bigger than small display
+    if (window.innerWidth > 768) {
+      this.clickButton(this.firstLevelButtonTargets[0])
+    }
   }
 
   toggle(event) {
     let button = event.currentTarget
+    this.clickButton(button)
+  }
+
+  clickButton(button) {
+    this.firstLevelButtonTargets.forEach(function(button) {
+      button.classList.remove('opacity-100')
+      button.classList.add('opacity-50')
+    })
+    button.classList.add('opacity-100')
+    button.classList.remove('opacity-50')
     let secondLevel = button.nextElementSibling
     let closed = secondLevel.classList.contains('translate-x-full')
     this.closeSecondLevels()
