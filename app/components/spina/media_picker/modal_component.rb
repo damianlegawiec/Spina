be04f196583539e2c2b5ace768bc7d8bcf5ebba3
Bram Jetten <mail@bramjetten.nl>
2020-11-26 14:53:13 +0100

    Image count in modal

--- app/components/spina/media_picker/modal_component.rb
+++ app/components/spina/media_picker/modal_component.rb
@@ -5,6 +5,10 @@ module Spina
       def media_folders
         @media_folders ||= Spina::MediaFolder.order(:name)
       end
+      
+      def image_count
+        @image_count ||= Spina::Image.count
+      end
 
     end
   end
