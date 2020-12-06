90da52722b0252de110f11179514f3d7de2261f3
Bram Jetten <mail@bramjetten.nl>
2020-12-06 14:50:58 +0100

    Upload from modal

--- app/controllers/spina/admin/media_picker/images_controller.rb
+++ app/controllers/spina/admin/media_picker/images_controller.rb
@@ -1,12 +1,33 @@
-module Spina::Admin
-  module MediaPicker
-    class ImagesController < AdminController
-
-      def index
-        @images = Spina::Image.order(created_at: :desc).page(params[:page]).per(25)
-        render layout: false
+module Spina
+  module Admin
+    module MediaPicker
+      class ImagesController < AdminController
+  
+        def index
+          @images = Spina::Image.order(created_at: :desc).page(params[:page]).per(25)
+          render layout: false
+        end
+        
+        # There's no file validation yet in ActiveStorage
+        # We do two things to reduce errors right now:
+        # 1. We add accept="image/*" to the image form
+        # 2. We destroy the entire record if the uploaded file is not an image
+        def create
+          sleep 1
+          @images = params[:image][:files].map do |file|
+            # Create the image and attach the file
+            image = Image.create(media_folder_id: params[:media_library])
+            image.file.attach(file)
+  
+            # Was it not an image after all? DESTROY IT
+            image.destroy and next unless image.file.image?
+  
+            image
+          end.compact
+          render Spina::MediaPicker::ImageComponent.with_collection(@images)
+        end
+  
       end
-
     end
   end
 end
