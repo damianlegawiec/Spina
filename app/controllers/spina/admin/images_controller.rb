1f8327128e8a185a37537b797486af4e67c813b0
Bram Jetten <mail@bramjetten.nl>
2020-12-08 21:57:39 +0100

    Imagescontroller

--- app/controllers/spina/admin/images_controller.rb
+++ app/controllers/spina/admin/images_controller.rb
@@ -18,7 +18,7 @@ module Spina
       def create
         @images = params[:image][:files].map do |file|
           # Create the image and attach the file
-          image = Image.create(media_folder_id: params[:media_library])
+          image = Image.create(media_folder_id: image_params[:media_folder_id])
           image.file.attach(file)
 
           # Was it not an image after all? DESTROY IT
