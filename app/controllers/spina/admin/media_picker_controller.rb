f1178d91f0343123b102d1f138ae392dfbaed1f6
Bram Jetten <mail@bramjetten.nl>
2020-12-13 22:34:30 +0100

    Use Modal mimetype instead of html

--- app/controllers/spina/admin/media_picker_controller.rb
+++ app/controllers/spina/admin/media_picker_controller.rb
@@ -3,7 +3,7 @@ module Spina
     class MediaPickerController < AdminController
       
       def show
-        render Spina::MediaPicker::ModalComponent.new, layout: false
+        render Spina::MediaPicker::ModalComponent.new
       end
       
     end
