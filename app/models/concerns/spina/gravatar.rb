8096fbc98d07742cc9f53ae8949537013e397b67
Bram Jetten <mail@bramjetten.nl>
2021-01-17 19:16:22 +0100

    Add fallback avatar

--- app/models/concerns/spina/gravatar.rb
+++ app/models/concerns/spina/gravatar.rb
@@ -1,14 +1,16 @@
-require 'digest'
-
 module Spina
   module Gravatar
     extend ActiveSupport::Concern
     
     def gravatar_url
-      "https://www.gravatar.com/avatar/#{gravatar_hash}?d=mp"
+      "https://www.gravatar.com/avatar/#{gravatar_hash}?d=#{fallback_url}"
     end
     
     private
+    
+      def fallback_url
+        "https://eu.ui-avatars.com/api/#{CGI.escape(name)}/128"
+      end
     
       def gravatar_hash
         Digest::MD5.hexdigest(email.to_s.strip.downcase)
