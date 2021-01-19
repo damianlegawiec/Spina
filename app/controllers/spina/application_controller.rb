a2cfe1f50fab92921401ae7e050092dfe95bd7aa
Bram Jetten <mail@bramjetten.nl>
2021-01-19 21:56:09 +0100

    Logout avatar

--- app/controllers/spina/application_controller.rb
+++ app/controllers/spina/application_controller.rb
@@ -12,7 +12,6 @@ module Spina
 
     def current_spina_user
       Spina::Current.user ||= ::Spina::User.where(id: cookies.signed[:spina_user_id]).first if cookies.signed[:spina_user_id]
-      @current_spina_user = Spina::User.first
     end
     helper_method :current_spina_user
 
