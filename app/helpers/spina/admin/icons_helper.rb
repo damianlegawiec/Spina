888c66e1dd82968ffaa3bf6d0b4632bac71d39ef
Bram Jetten <mail@bramjetten.nl>
2021-01-23 19:55:56 +0100

    Update heroicon helper and turbo_stream

--- app/helpers/spina/admin/icons_helper.rb
+++ app/helpers/spina/admin/icons_helper.rb
@@ -6,15 +6,12 @@ module Spina::Admin
 		class FileNotFound < StandardError
 	  end
 		
-		def heroicon(name, style: :outline, size: nil, **options)
+		def heroicon(name, style: :outline, **options)
 	    file = read_file(Spina::Engine.root.join("app/assets/icons/heroicons", style.to_s, "#{name}.svg"))
-	    
-	    size ||= style == :outline ? 6 : 5
 	    
 	    doc = Nokogiri::XML(file)
 	    svg = doc.root
-	    classes = ["h-#{size}", "w-#{size}", options[:class]].compact
-	    svg[:class] = classes.join(" ")
+	    svg[:class] = options[:class]
 	    ActiveSupport::SafeBuffer.new(svg.to_s)
 	  end
 		
