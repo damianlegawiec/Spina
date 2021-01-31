85ff385b6728dffe81dede65f826111e08b5a3e3
Bram Jetten <mail@bramjetten.nl>
2021-01-31 11:21:40 +0100

    Fadein if not loaded yet

--- app/assets/javascripts/spina/controllers/image_fade_in_controller.js
+++ app/assets/javascripts/spina/controllers/image_fade_in_controller.js
@@ -3,12 +3,18 @@ import { Controller } from "stimulus"
 export default class extends Controller {
 
   connect() {
-    this.element.classList.add('transition-opacity', 'duration-500', 'opacity-0')
-    this.element.addEventListener("load", this.fadeIn.bind(this))
+    if (!this.loaded) {
+      this.element.classList.add('transition-opacity', 'duration-500', 'opacity-0')
+      this.element.addEventListener("load", this.fadeIn.bind(this))
+    }
   }
 
   fadeIn() {
     this.element.classList.remove('opacity-0')
+  }
+  
+  get loaded() {
+    return this.element.complete && this.element.naturalHeight !== 0
   }
 
 }
